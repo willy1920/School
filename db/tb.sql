a2db1f2973514f99596e00567611e0dd559da8a3
Willy <willychai05@gmail.com>
2017-08-25 08:13:22 +0700

    fix database

--- db/tb.sql
+++ db/tb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 23 Agu 2017 pada 14.50
+-- Generation Time: 25 Agu 2017 pada 03.12
 -- Versi Server: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -33,6 +33,13 @@ CREATE TABLE `class` (
   `name` char(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `class`
+--
+
+INSERT INTO `class` (`id`, `name`) VALUES
+(1, 'P1 Curiosity');
+
 -- --------------------------------------------------------
 
 --
@@ -40,20 +47,20 @@ CREATE TABLE `class` (
 --
 
 CREATE TABLE `dad` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `name` char(50) DEFAULT NULL,
-  `place_birth` char(30) DEFAULT NULL,
-  `date_birth` date DEFAULT NULL,
-  `job` char(30) DEFAULT NULL,
-  `address` char(60) DEFAULT NULL,
-  `office` char(60) DEFAULT NULL,
-  `phone_number` char(12) DEFAULT NULL,
-  `wa` char(12) DEFAULT NULL,
-  `bbm` char(10) DEFAULT NULL,
-  `email` char(30) DEFAULT NULL,
-  `last_education` char(20) DEFAULT NULL,
-  `nationality` char(20) DEFAULT NULL,
-  `religion` char(10) DEFAULT NULL
+  `dad_id` smallint(5) UNSIGNED NOT NULL,
+  `dad_name` char(50) DEFAULT NULL,
+  `dad_place_birth` char(30) DEFAULT NULL,
+  `dad_date_birth` date DEFAULT NULL,
+  `dad_job` char(30) DEFAULT NULL,
+  `dad_address` char(60) DEFAULT NULL,
+  `dad_office` char(60) DEFAULT NULL,
+  `dad_phone_number` char(12) DEFAULT NULL,
+  `dad_wa` char(12) DEFAULT NULL,
+  `dad_bbm` char(10) DEFAULT NULL,
+  `dad_email` char(30) DEFAULT NULL,
+  `dad_last_education` char(20) DEFAULT NULL,
+  `dad_nationality` char(20) DEFAULT NULL,
+  `dad_religion` char(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,21 +81,21 @@ CREATE TABLE `generation` (
 --
 
 CREATE TABLE `guardian` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `name` char(50) DEFAULT NULL,
-  `place_birth` char(30) DEFAULT NULL,
-  `date_birth` date DEFAULT NULL,
-  `job` char(30) DEFAULT NULL,
-  `address` char(60) DEFAULT NULL,
-  `office` char(60) DEFAULT NULL,
-  `phone_number` char(12) DEFAULT NULL,
-  `wa` char(12) DEFAULT NULL,
-  `bbm` char(10) DEFAULT NULL,
-  `email` char(30) DEFAULT NULL,
-  `last_education` char(20) DEFAULT NULL,
-  `nationality` char(20) DEFAULT NULL,
-  `religion` char(11) DEFAULT NULL,
-  `relationship` char(20) DEFAULT NULL
+  `guardian_id` smallint(5) UNSIGNED NOT NULL,
+  `mom_name` char(50) DEFAULT NULL,
+  `mom_place_birth` char(30) DEFAULT NULL,
+  `guardian_date_birth` date DEFAULT NULL,
+  `guardian_job` char(30) DEFAULT NULL,
+  `guardian_address` char(60) DEFAULT NULL,
+  `guardian_office` char(60) DEFAULT NULL,
+  `guardian_phone_number` char(12) DEFAULT NULL,
+  `guardian_wa` char(12) DEFAULT NULL,
+  `guardian_bbm` char(10) DEFAULT NULL,
+  `guardian_email` char(30) DEFAULT NULL,
+  `guardian_last_education` char(20) DEFAULT NULL,
+  `guardian_nationality` char(20) DEFAULT NULL,
+  `guardian_religion` char(10) DEFAULT NULL,
+  `guardian_relationship` char(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -98,20 +105,20 @@ CREATE TABLE `guardian` (
 --
 
 CREATE TABLE `mom` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `name` char(50) DEFAULT NULL,
-  `place_birth` char(30) DEFAULT NULL,
-  `date_birth` date DEFAULT NULL,
-  `job` char(30) DEFAULT NULL,
-  `address` char(60) DEFAULT NULL,
-  `office` char(60) DEFAULT NULL,
-  `phone_number` char(12) DEFAULT NULL,
-  `wa` char(12) DEFAULT NULL,
-  `bbm` char(10) DEFAULT NULL,
-  `email` char(30) DEFAULT NULL,
-  `last_education` char(20) DEFAULT NULL,
-  `nationality` char(20) DEFAULT NULL,
-  `religion` char(10) DEFAULT NULL
+  `mom_id` smallint(5) UNSIGNED NOT NULL,
+  `mom_name` char(50) DEFAULT NULL,
+  `mom_place_birth` char(30) DEFAULT NULL,
+  `mom_date_birth` date DEFAULT NULL,
+  `mom_job` char(30) DEFAULT NULL,
+  `mom_address` char(60) DEFAULT NULL,
+  `mom_office` char(60) DEFAULT NULL,
+  `mom_phone_number` char(12) DEFAULT NULL,
+  `mom_wa` char(12) DEFAULT NULL,
+  `mom_bbm` char(10) DEFAULT NULL,
+  `mom_email` char(30) DEFAULT NULL,
+  `mom_last_education` char(20) DEFAULT NULL,
+  `mom_nationality` char(20) DEFAULT NULL,
+  `mom_religion` char(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -134,6 +141,15 @@ CREATE TABLE `others` (
   `tanggal_diterima` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `others`
+--
+
+INSERT INTO `others` (`id`, `WN`, `asal_sekolah`, `alamat_asal_sekolah`, `kelas_pertama`, `jumlah_saudara`, `anak_ke`, `status`, `bahasa`, `agama`, `tanggal_diterima`) VALUES
+(3, 'Indonesia', 'Tk Tunas Bangsa', 'Jl. Arteri Km2', '1', 3, 1, NULL, 'Indonesia', 'Kristen', '2017-05-07'),
+(4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Indonesia', 'Gembala Baik', 'Jl. Tanjungpura', '1', 3, 1, NULL, 'Indonesia', 'Kristen', '2017-05-12');
+
 -- --------------------------------------------------------
 
 --
@@ -153,6 +169,14 @@ CREATE TABLE `staf` (
   `bbm` char(12) DEFAULT NULL,
   `status` char(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `staf`
+--
+
+INSERT INTO `staf` (`id`, `name`, `hire_date`, `end_contract`, `place_birth`, `date_birth`, `email`, `phone_number`, `wa`, `bbm`, `status`) VALUES
+(3, 'Willy', '2017-08-02', '2017-08-31', 'Singkawang', '1996-10-05', 'willychai05@gmail.com', '08985935425', '08985935425', NULL, 'ICT'),
+(4, 'rina', '2017-08-08', '2017-08-31', 'asdf', '2017-08-24', NULL, NULL, NULL, NULL, 'TU');
 
 -- --------------------------------------------------------
 
@@ -185,6 +209,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `pass`) VALUES
+(3, 'willychai05@gmail.com', '23f3094a2cd4633873918960bef54ab6cd7f3340'),
+(4, 'rina', '70e21878d268fa8f82817f9278f8bae0fb108950');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -198,7 +230,7 @@ ALTER TABLE `class`
 -- Indexes for table `dad`
 --
 ALTER TABLE `dad`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`dad_id`);
 
 --
 -- Indexes for table `generation`
@@ -210,13 +242,13 @@ ALTER TABLE `generation`
 -- Indexes for table `guardian`
 --
 ALTER TABLE `guardian`
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`guardian_id`);
 
 --
 -- Indexes for table `mom`
 --
 ALTER TABLE `mom`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`mom_id`);
 
 --
 -- Indexes for table `others`
@@ -256,12 +288,12 @@ ALTER TABLE `class`
 -- AUTO_INCREMENT for table `generation`
 --
 ALTER TABLE `generation`
-  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `others`
 --
 ALTER TABLE `others`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `staf`
 --
@@ -271,7 +303,7 @@ ALTER TABLE `staf`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
@@ -280,19 +312,19 @@ ALTER TABLE `students`
 -- Ketidakleluasaan untuk tabel `dad`
 --
 ALTER TABLE `dad`
-  ADD CONSTRAINT `dad_ibfk_1` FOREIGN KEY (`id`) REFERENCES `students` (`id`);
+  ADD CONSTRAINT `dad_ibfk_1` FOREIGN KEY (`dad_id`) REFERENCES `students` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `guardian`
 --
 ALTER TABLE `guardian`
-  ADD CONSTRAINT `guardian_ibfk_1` FOREIGN KEY (`id`) REFERENCES `students` (`id`);
+  ADD CONSTRAINT `guardian_ibfk_1` FOREIGN KEY (`guardian_id`) REFERENCES `students` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `mom`
 --
 ALTER TABLE `mom`
-  ADD CONSTRAINT `mom_ibfk_1` FOREIGN KEY (`id`) REFERENCES `students` (`id`);
+  ADD CONSTRAINT `mom_ibfk_1` FOREIGN KEY (`mom_id`) REFERENCES `students` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `others`
